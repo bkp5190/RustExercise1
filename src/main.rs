9c1f33e94f595666f84dafa7ab8a0d3a1e874166
Boney Patel <boneypatel37@yahoo.com>
2023-10-20 21:43:41 -0400

    Second part of exercise complete

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,11 @@
 fn main() {
-    let missiles: i32 = 8;
+    let mut missiles: i32 = 8;
 
     let ready: i32 = 2;
 
     println!("Firing {} of my {} missiles...", ready, missiles);
+
+    missiles = missiles - ready;
+
+    println!("{} missiles left", missiles);
 }
